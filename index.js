eb0cf19b06a93966e32cfc04bc964aae639b1ccb
ReginaldoSR37 <updatingdevelop37.com>
2024-04-14 19:50:48 -0300

    estudando conceito de rotas HTTPS

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+const port = 3000
+const app = express()
+app.use(express.json())
+
+app.get('/users/',(request,response)=>{
+        console.log(request.body)
+
+        
+return response.json({message: "ok"})
+     
+})
+        app.listen(port,()=>{
+            console.log(`🎸server started on port ${port} 😎`)
+
+            
+        })
+
